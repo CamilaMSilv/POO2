2956dbfb2bbe0b92b6eedcc406ea414515169e73
Camila Martins <camiladeveloper@hotmail.com>
2023-05-22 20:36:53 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,71 +1,132 @@
 "use strict";
-class Funcionario {
-    constructor(nome, codigo) {
-        this.nome = nome;
-        this.codigo = codigo;
+/*nome: Camila
+Sua tarefa e desenvolver um sistema que faça o controle de velocidade de uma
+rodovia. O sistema deve atender os seguintes requisitos:
+- Ter pelo menos 3 classes.
+- O limite de velocidade na rodovia e de 100 km/h.
+- A tolerância para infração de é de 5%.
+- Utilizar o conceito de herança, encapsulamento e polimorfismo.
+- Notificar o motorista sobre a infração.
+- O sistema deve funcionar.
+*/
+class Veiculo {
+    constructor(id, placa, proprietario) {
+        this._id = id;
+        this._placa = placa;
+        this._proprietario = proprietario;
+    }
+    get id() {
+        return this._id;
+    }
+    set id(id) {
+        this._id = id;
+    }
+    get placa() {
+        return this._placa;
+    }
+    set placa(placa) {
+        this._placa = placa;
+    }
+    get proprietario() {
+        return this._proprietario;
+    }
+    set proprietario(proprietario) {
+        this._proprietario = proprietario;
     }
 }
-class Ponto {
-    constructor(funcionario) {
-        this.funcionario = funcionario;
-        this.horarioEntrada = null;
-        this.horarioSaida = null;
-        this.horarioInicioIntervalo = null;
-        this.horarioFimIntervalo = null;
+class Pessoa {
+    constructor(id, nome, cpf, cnh) {
+        this._id = id;
+        this._nome = nome;
+        this._cpf = cpf;
+        this._cnh = cnh;
     }
-    registrarPonto(codigo) {
-        console.log("1 " + this.funcionario.codigo);
-        console.log("2 " + this.funcionario.codigo);
-        console.log("3 " + (this.funcionario.codigo === codigo));
-        if (this.funcionario.codigo === codigo) {
-            const dataHoraAtual = new Date;
-            const hora = dataHoraAtual.getHours();
-            const minutos = dataHoraAtual.getMinutes();
-            console.log("4 " + hora);
-            console.log("5 " + minutos);
-            console.log("6 " + dataHoraAtual);
-            if (!this.horarioEntrada && (hora < 18 || (hora == 18 && minutos <= 5))) {
-                this.horarioEntrada = dataHoraAtual;
-                console.log(`Ponto de entrada registrado para o funcionário ${this.funcionario.nome} na data ${dataHoraAtual.toLocaleDateString()} às ${dataHoraAtual.toLocaleTimeString()}`);
-            }
-            else if (this.horarioEntrada && !this.horarioInicioIntervalo) {
-                this.horarioInicioIntervalo = dataHoraAtual;
-                console.log(`Ponto de início de intervalo registrado para o funcionário ${this.funcionario.nome} na data ${dataHoraAtual.toLocaleDateString()} às ${dataHoraAtual.toLocaleTimeString()}`);
-            }
-            else if (this.horarioInicioIntervalo && !this.horarioFimIntervalo
-                && (dataHoraAtual.getTime() - this.horarioInicioIntervalo.getTime()) >= 3600000
-                && (dataHoraAtual.getTime() - this.horarioInicioIntervalo.getTime()) <= 5400000) {
-                //&& (horario.getHours() - this.horarioInicioIntervalo.getHours()) == 1
-                //&& (horario.getMinutes() - this.horarioInicioIntervalo.getMinutes()) <= 30) {
-                this.horarioFimIntervalo = dataHoraAtual;
-                console.log(`Ponto de fim de intervalo registrado para o funcionário ${this.funcionario.nome} na data ${dataHoraAtual.toLocaleDateString()} às ${dataHoraAtual.toLocaleTimeString()}`);
-            }
-            else if (this.horarioFimIntervalo && !this.horarioSaida &&
-                ((hora == 21 && minutos >= 55) || hora > 21)) {
-                this.horarioSaida = dataHoraAtual;
-                console.log(`Ponto de saída registrado para o funcionário ${this.funcionario.nome} na data ${dataHoraAtual.toLocaleDateString()} às ${dataHoraAtual.toLocaleTimeString()}`);
-            }
-            else {
-                console.log(`Ponto não registrado para o funcionário ${this.funcionario.nome} na data ${dataHoraAtual.toLocaleDateString()} às ${dataHoraAtual.toLocaleTimeString()}`);
-            }
+    get id() {
+        return this._id;
+    }
+    set id(id) {
+        this._id = id;
+    }
+    get nome() {
+        return this._nome;
+    }
+    set nome(nome) {
+        this._nome = nome;
+    }
+    get cpf() {
+        return this._cpf;
+    }
+    set cpf(cpf) {
+        this._cpf = cpf;
+    }
+    get cnh() {
+        return this._cnh;
+    }
+    set cnh(cnh) {
+        this._cnh = cnh;
+    }
+}
+class Registro {
+    constructor(id, veiculo, motorista, velocidade, dataHora) {
+        this._id = id;
+        this._veiculo = veiculo;
+        this._motorista = motorista;
+        this._velocidade = velocidade;
+        this._dataHora = dataHora;
+    }
+    verificarVelocidade() {
+        const limiteVelocidade = 100;
+        const tolerancia = 1.05; //+5%
+        const velocidadeMaxima = limiteVelocidade * tolerancia;
+        if (this.velocidade > velocidadeMaxima) {
+            console.log(`O veículo com placa ${this.veiculo.placa} está acima do limite de velocidade.`);
+            this.notificarMotorista();
+        }
+        else {
+            console.log(`O veículo com placa ${this.veiculo.placa} está dentro do limite de velocidade.`);
         }
     }
-    calcularSaldo() {
-        if (this.horarioEntrada && this.horarioSaida && this.horarioInicioIntervalo && this.horarioFimIntervalo) {
-            const dataHoraAtual = new Date;
-            const primeiraHora = this.horarioInicioIntervalo.getTime() - this.horarioEntrada.getTime();
-            const segundaHora = this.horarioSaida.getTime() - this.horarioFimIntervalo.getTime();
-            const saldoHora = millisToHoursAndMinutes(primeiraHora + segundaHora);
-            console.log(`Saldo de horas trabalhadas igual a ${saldoHora} para o funcionário ${this.funcionario.nome} na data ${dataHoraAtual.toLocaleDateString()} às ${dataHoraAtual.toLocaleTimeString()}`);
-        }
+    notificarMotorista() {
+        console.log(`Notificação de Infração: Você excedeu o limite de velocidade de 100km/h!\n`
+            + ` Placa: ${this.veiculo.placa} Proprietário: ${this.veiculo.proprietario.nome}\n`
+            + ` Motorista: ${this.motorista.nome} CPF: ${this.motorista.cpf}\n`
+            + ` Data/Hora: ${this.dataHora.toLocaleDateString()} às ${this.dataHora.toLocaleTimeString()} Velocidade: ${this.velocidade}\n`);
+    }
+    get id() {
+        return this._id;
+    }
+    set id(id) {
+        this._id = id;
+    }
+    get veiculo() {
+        return this._veiculo;
+    }
+    set veiculo(veiculo) {
+        this._veiculo = veiculo;
+    }
+    get motorista() {
+        return this._motorista;
+    }
+    set motorista(motorista) {
+        this._motorista = motorista;
+    }
+    get velocidade() {
+        return this._velocidade;
+    }
+    set velocidade(velocidade) {
+        this._velocidade = velocidade;
+    }
+    get dataHora() {
+        return this._dataHora;
+    }
+    set dataHora(dataHora) {
+        this._dataHora = dataHora;
     }
 }
-function millisToHoursAndMinutes(millis) {
-    const hours = Math.floor(millis / 3600000); // Divide o valor em milissegundos por 3600000 para obter as horas inteiras
-    const minutes = Math.floor((millis % 3600000) / 60000); // Obtém o resto da divisão por 3600000 e divide por 60000 para obter os minutos
-    return { hours, minutes };
-}
-var funcionario = new Funcionario("Camila", "000001");
-var ponto = new Ponto(funcionario);
-ponto.registrarPonto("000001");
-ponto.calcularSaldo();
+var proprietario = new Pessoa(1, "Márcio José da Silva", "468.435.300-10", "02127708593");
+var veiculo = new Veiculo(1, "MGV4472", proprietario);
+var motorista = new Pessoa(2, "Camila Martins da Silva", "963.452.790-64", "69965019730");
+var registro = new Registro(1, veiculo, motorista, 130, new Date);
+registro.verificarVelocidade();
+var registro2 = new Registro(2, veiculo, proprietario, 105, new Date);
+registro2.verificarVelocidade();
